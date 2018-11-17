cadd90bab421619455c5da754bb179a6f7f5ccc8
mark <hdmark67@gmail.com>
2018-11-17 18:26:18 -0500

    starting mixins

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+  browserSync.init({
+    open: false,
+    notify: false,
+    server: {
+      baseDir: 'app',
+    },
+  });
+  watch('./app/index.html', () => {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], () => {
+  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream());
+});
